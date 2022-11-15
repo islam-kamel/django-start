dced35847d7b78f1f05de8cb0367cfd058705f04
islam-kamel <islam.kamel@agr.svu.edu.eg>
2022-11-15 10:51:14 +0200

    Refactor Code

--- djstartlib/models/project_manager.py
+++ djstartlib/models/project_manager.py
@@ -3,7 +3,6 @@ import os
 import click
 
 from models.utils import Environment
-# from app_manager import AppManager
 from models.utils.hellper import (executable_django_command, install_dep,
                                   requirements_extract, upgrade_pip,
                                   warn_stdout)
@@ -12,96 +11,56 @@ from models.utils.hellper import (executable_django_command, install_dep,
 class ProjectManager(Environment):
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        # self.__app_manager = AppManager(*args, **kwargs)
-        self.__workdir = os.getcwd()
-        self.__app_name = kwargs.get("app", None)
-        self.__core_name = kwargs.get("project", None)
-        self.__urls = self.__workdir + rf"{os.sep}{self.core_name}{os.sep}urls.py"
-        self.__settings = (
-            self.__workdir + rf"{os.sep}{self.core_name}{os.sep}settings.py"
+        self.__urls = os.path.join(
+            self.get_workdir(), f'{self.get_project_name()}/urls.py'
         )
-        self.python = os.environ.get("PYTHONPATH", None)
-        self.django_admin = os.environ.get("DJANGOADMIN", None)
+        self.__settings = os.path.join(
+            self.get_workdir(), f'{self.get_project_name()}/settings.py'
+        )
         self.__other_dependencies = []
         self.__line_list = []
-    """
-    @property
-    def app_manager(self):
-        return self.__app_manager
-    """
-    @property
-    def python(self):
-        return self.__python
-
-    @python.setter
-    def python(self, python_path):
-        self.__python = python_path
 
     @property
-    def django_admin(self):
-        return self.__django_admin
-
-    @django_admin.setter
-    def django_admin(self, django_admin_path):
-        self.__django_admin = django_admin_path
+    def urls_path(self) -> str: return self.__urls
 
     @property
-    def core_name(self):
-        return self.__core_name
+    def settings_path(self) -> str: return self.__settings
 
     @property
-    def workdir(self):
-        return self.__workdir
+    def line_list(self) -> list: return self.__line_list
 
-    @property
-    def app_name(self):
-        return self.__app_name
+    def index(self, value: str) -> int: return self.__line_list.index(value)
 
-    @property
-    def urls_path(self):
-        return self.__urls
+    def update_lines_list(self, flag: str, value: str) -> None:
+        self.__line_list.insert(self.index(flag), value)
 
-    @property
-    def settings_path(self):
-        return self.__settings
-
-    @property
-    def line_list(self):
-        return self.__line_list
+    def replace_line(self, index, value) -> None:
+        self.__line_list[index] = value
 
     @line_list.setter
-    def line_list(self, value):
+    def line_list(self, value: list) -> None:
         self.__line_list = value
 
-    def index(self, value):
-        return self.__line_list.index(value)
-
-    def read_file(self, file):
+    def read_file(self, file: str) -> list:
         with open(file) as f:
             self.__line_list = f.readlines()
             f.close()
         return self.__line_list
 
-    def update_lines_list(self, flag, value):
-        self.__line_list.insert(self.index(flag), value)
-
-    def replace_line(self, index, value):
-        self.__line_list[index] = value
-
-    def update_settings(self):
+    def update_settings(self) -> None:
         self.read_file(self.settings_path)
-        if f"\t'{self.app_name}',\n" not in self.__line_list:
+        if f"\t'{self.get_app_name()}',\n" not in self.__line_list:
             click.secho("\U0001F527 Update Project Settings...", fg="blue")
-            self.update_lines_list("]\n", f"\t'{self.app_name}',\n")
+            self.update_lines_list("]\n", f"\t'{self.get_app_name()}',\n")
             with open(self.settings_path, "w") as f:
                 f.write("".join(self.__line_list))
                 f.close()
         else:
-            warn_stdout(f'"{self.app_name}" is installed!')
+            warn_stdout(f'"{self.get_app_name()}" is installed!')
 
-    def update_urls(self):
+    def update_urls(self) -> None:
         self.read_file(self.urls_path)
-        view_path = f"\tpath('', include('{self.app_name}.urls')),\n"
+        view_path = f"\tpath('', include('{self.get_app_name()}.urls')),\n"
         if view_path not in self.__line_list:
             self.replace_line(
                 self.index("from django.urls import path\n"),
@@ -115,23 +74,23 @@ class ProjectManager(Environment):
             warn_stdout("Urls Already Updated")
 
     @staticmethod
-    def upgrade_pip():
+    def upgrade_pip() -> None:
         click.secho("\U0001F4E6 Upgrade Pip...", fg="blue")
         upgrade_pip()
 
     @staticmethod
-    def install_dep():
+    def install_dep() -> None:
         click.secho("\U000023F3 Install Dependencies...", fg="blue")
         install_dep()
 
     @staticmethod
-    def requirements_extract():
+    def requirements_extract() -> None:
         click.secho("\U0001F4C3 Generate Requirements.txt...", fg="blue")
         requirements_extract()
 
-    def create_project(self):
-        if self.core_name not in os.listdir(self.workdir):
-            click.secho(f"\U00002728 Create '{self.core_name}' Project", fg="blue")
-            executable_django_command(f"startproject {self.core_name} .")
+    def create_project(self) -> None:
+        if self.get_project_name() not in os.listdir(self.get_workdir()):
+            click.secho(f"\U00002728 Create '{self.get_project_name()}' Project", fg="blue")
+            executable_django_command(f"startproject {self.get_project_name()} .")
         else:
-            warn_stdout(f'"{self.core_name}" already exist!')
+            warn_stdout(f'"{self.get_project_name()}" Already exist!')
